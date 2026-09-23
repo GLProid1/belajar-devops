f15757276dece3dd186ef4efcf1dc2acc45dc50c
GLProid1 <riconatanael2212@gmail.com>
2026-09-23 10:38:47 +0700

    memperbaiki indentasi

--- kalkulator.py
+++ kalkulator.py
@@ -1,22 +1,22 @@
 """Modul sederhana untuk demostrasi Pylint Quality Gate"""
 
 def hitung_luas_persegi_panjang(panjang: int, lebar: int) -> int:
-  """Fungsi untuk menghitung luas persegi panjang
+    """Fungsi untuk menghitung luas persegi panjang
   
-  Args:
-    panjang (int): Panjang sisi persegi panjang.
-    lebar (int): Lebar sisi persegi panjang.
+    Args:
+      panjang (int): Panjang sisi persegi panjang.
+      lebar (int): Lebar sisi persegi panjang.
     
-  Returns:
-    int: Hasil perkalian panjang dan lebar
+    Returns:
+        int: Hasil perkalian panjang dan lebar
   """
   
-  return (panjang * lebar)
+    return (panjang * lebar)
 
 def main():
-  """Fungsi utama untuk menjalankan program"""
-  hasil = hitung_luas_persegi_panjang(4, 10)
-  print(f"Luas persegi panjang adalah: {hasil}")
+    """Fungsi utama untuk menjalankan program"""
+    hasil = hitung_luas_persegi_panjang(4, 10)
+    print(f"Luas persegi panjang adalah: {hasil}")
   
 if __name__ == "__main__":
-  main()
+    main()
